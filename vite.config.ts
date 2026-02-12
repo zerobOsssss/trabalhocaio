08a16117432bac1697862b8d687b2a85c186a145
zerobOsssss <deuspetrik@gmail.com>
2026-02-11 22:56:37 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,19 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+  // Carrega variáveis de ambiente baseadas no modo atual
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    plugins: [react()],
+    define: {
+      // Isso permite que o código existente `process.env.API_KEY` continue funcionando
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    },
+    build: {
+      outDir: 'dist',
+    }
+  };
 });
